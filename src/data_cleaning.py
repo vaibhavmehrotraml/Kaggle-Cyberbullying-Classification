c1356cf037a4079e39abd88b0caca5e39b2d34d1
Vaibhav Mehrotra <vaibhav1@uw.edu>
2023-09-09 01:02:45 +0530

    refactor(src): Clean up and optimize

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -5,7 +5,9 @@ Arguments are fed in with argparser.
 import pandas as pd
 import logging
 import argparse
+
 logging.basicConfig(filename='preprocessing.log', level=logging.INFO,
+                    filemode='w',
                     format='%(asctime)s %(levelname)-8s %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S'
                     )
@@ -14,7 +16,7 @@ logging.basicConfig(filename='preprocessing.log', level=logging.INFO,
 parser = argparse.ArgumentParser(
     description="Preprocess the given files using the given arguments.",
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("-f", "--file",
+parser.add_argument("-f", "--input-file",
                     action='store',
                     dest='filename',
                     type=str,
@@ -22,44 +24,52 @@ parser.add_argument("-f", "--file",
 parser.add_argument("-o", "--output",
                     action='store',
                     dest='output',
+                    required=True,
                     type=str,
                     help="Define the file path of the cleaned datafile.")
-parser.add_argument("-m", "--max_char",
+parser.add_argument("-m", "--min-char",
                     action='store',
-                    dest='max_char',
+                    dest='min_char',
                     type=int,
-                    default=300,
-                    help="Define the maximum length of a given tweet.")
+                    default=100,
+                    help="Define the minimum length of a given tweet.")
+
 
 if __name__ == '__main__':
-    logging.info(f"{'###'*5} Cleaning Input Tweets File {'###'*5}")
+    logging.info(f"{'###' * 5} Cleaning Input Tweets File {'###' * 5}")
 
     # Parse arguments
     args = parser.parse_args()
     filename = args.filename
     output_file = args.output
-    max_char = args.max_char
+    min_char = args.min_char
 
     # Load data
     df = pd.read_csv(filename)
 
     # Clean data
     logging.info(f"Raw data has {df.shape[0]} rows and {df.shape[1]} columns.")
-    logging.info(f"\tColumns are {list(df.columns)}")
+    logging.info(f"Columns are {list(df.columns)}")
+
     # Drop null data
     prev = df.shape[0]
-    df = df[~df.tweet_text.isna()]
-    df = df[~df.cyberbullying_type.isna()]
+    df = df.dropna()
     logging.info(f"Removing null tweets & cyberbullying classes reduced data by {(1-(df.shape[0]/prev)):.2%}.")
+
+    # Removing tweets above threshold of characters
     prev = df.shape[0]
-    df = df[df.tweet_text.str.len() < max_char]
-    logging.info(f"Removing tweets that exceed character limit reduced data by {(1-(df.shape[0]/prev)):.2%}.")
+    df = df[df.tweet_text.str.len() > min_char]
+    logging.info(f"Removing tweets that have less than {min_char} chars, reduced data by {(1-(df.shape[0]/prev)):.2%}.")
+
+    # Removing @ twitter-handles
     prev = df.shape[0]
-    df['tweet_text'] = df.tweet_text.apply(
-        lambda x: ' '.join(word for word in x.split(' ') if not word.startswith('@')))
+    df['tweet_text'] = df.tweet_text.str.replace(r'@\w+', '', regex=True)
+
+    # Remove unnecessary class and duplicates
     df = df[df.cyberbullying_type != 'other_cyberbullying']
     df = df[~df.tweet_text.duplicated()]
     df = df[~(df.tweet_text == '')]
+
     logging.info(f"Removing empty, duplicated, or 'other cyberbullying' tweets reduced data by {(1-(df.shape[0]/prev)):.2%}.")
     logging.info(f"Final data has {df.shape[0]} rows and {df.shape[1]} columns.")
 
